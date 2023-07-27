ed16fc39e421e49fe90c8f7aea6840d948d13480
Omar Arias <omar.arias.dev@gmail.com>
2023-07-27 12:35:21 -0600

    Adding Captcha and link and POST request to send mail to email

--- src/screens/portfolio/components/ContactMeForm.jsx
+++ src/screens/portfolio/components/ContactMeForm.jsx
@@ -1,19 +1,63 @@
+import { useState, useRef } from "react";
+
+import ReCAPTCHA from "react-google-recaptcha";
+
 import IconSend from "../../../assets/images/components/genericIcons/IconSend";
 
 import "./../index.css";
 
 export default function ContactMeForm() {
+
+
+    const [validCaptcha, changeValidCaptcha] = useState(null);
+    const captcha = useRef(null);
+    
+    const onChange = () => {
+        if (captcha.current.getValue()) {
+            changeValidCaptcha(true);
+        }
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (captcha.current.getValue()) {
+            changeValidCaptcha(true);
+        } else {
+            changeValidCaptcha(false);
+        }
+        if (validCaptcha === true) {
+            event.target.submit();
+        }
+    }
+
     return(
-        <form className="foo">
+        <form
+            className="foo"
+            autoComplete="off"
+            action="https://formsubmit.co/omar.arias.dev@gmail.com"
+            method="POST"
+            onSubmit={(e) => handleSubmit(e)}
+        >
             <div className="contact_me-form">
                 <div className="form-inputs-container">
-                    <input className="form-text" type="text" placeholder="Name" required />
-                    <input className="form-text" type="email" placeholder="Email" required />
-                    <input className="form-text" type="text" placeholder="Subject" required />
+                    <input type="hidden" name="_captcha" value="false" />
+                    <input type="hidden" name="_next" value="https://www.linkedin.com/in/omar-arias-dev/" />
+                    <input className="form-text" type="text" placeholder="Name" name="name" required />
+                    <input className="form-text" type="email" placeholder="Email" name="email" required />
+                    <input className="form-text" type="text" placeholder="Subject" name="subject" required />
                 </div>
-                <textarea className="form-text form-text-message" placeholder="Message" required />
+                <textarea className="form-text form-text-message" placeholder="Message" name="message" required />
             </div>
             <div className="contact_me-form-button-container">
+                <ReCAPTCHA
+                    ref={captcha}
+                    sitekey="6LdeQ10nAAAAAJmyOu9rZ4BKK42LGMWIAlU1CNW_"
+                    onChange={onChange}
+                    theme="dark"
+                />
+                {
+                    validCaptcha === false && <p className="form-captcha-message">Do the captcha</p>
+                }
                 <button className="form-button-submit">
                     Send
                     <IconSend fill="white" />
